7cf6d2f186ea88f8e5fe1ed4dd83d7aa591d1ae7
samson-onyebuchi <nnadisamson63@gmail.com>
2023-09-09 10:48:29 +0100

    return all the countries digit code using pycountry and phonenumbers module

--- app/digit_code.py
+++ app/digit_code.py
@@ -1,69 +1,36 @@
 from flask import Flask, jsonify
+import pycountry
+import phonenumbers
 
 app = Flask(__name__)
 
-# Sample data for country dialing codes
-country_phone_codes = {
-    "United States": "+1",
-    "Canada": "+1",
-    "United Kingdom": "+44",
-    "Australia": "+61",
-    "India": "+91",
-    "China": "+86",
-    "Germany": "+49",
-    "France": "+33",
-    "Japan": "+81",
-    "Brazil": "+55",
-    "Russia": "+7",
-    "South Africa": "+27",
-    "Mexico": "+52",
-    "Argentina": "+54",
-    "Spain": "+34",
-    "Italy": "+39",
-    "Netherlands": "+31",
-    "Sweden": "+46",
-    "Norway": "+47",
-    "Switzerland": "+41",
-    "Singapore": "+65",
-    "Malaysia": "+60",
-    "New Zealand": "+64",
-    "Saudi Arabia": "+966",
-    "United Arab Emirates": "+971",
-    "South Korea": "+82",
-    "Egypt": "+20",
-    "Turkey": "+90",
-    "Greece": "+30",
-    "Thailand": "+66",
-    "Philippines": "+63",
-    "Indonesia": "+62",
-    "Vietnam": "+84",
-    "Pakistan": "+92",
-    "Bangladesh": "+880",
-    "Sri Lanka": "+94",
-    "Nepal": "+977",
-    "Afghanistan": "+93",
-    "Iraq": "+964",
-    "Iran": "+98",
-    "Kenya": "+254",
-    "Nigeria": "+234",
-    "Ghana": "+233",
-    "Morocco": "+212",
-    "Tunisia": "+216",
-    "Algeria": "+213",
-    "Chile": "+56",
-    "Peru": "+51",
-    "Colombia": "+57",
-    "Venezuela": "+58",
-    "Ecuador": "+593",
-    "Bolivia": "+591",
-    "Paraguay": "+595",
-    "Uruguay": "+598",
-    # Add more countries as needed
-}
+def generate_country_data():
+    COUNTRY_DATA = {}
 
+    for country in pycountry.countries:
+        country_code = country.alpha_2
 
-@app.route('/country-codes', methods=['GET'])
-def get_country_codes():
-    return jsonify(country_phone_codes)
+        country_data = {'code': country_code, 'calling_code': ''}
 
+        if hasattr(country, 'calling_codes'):
+            calling_codes = country.calling_codes
+            if calling_codes:
+                country_data['calling_code'] = calling_codes[0]
+        else:
+            try:
+                country_data['calling_code'] = phonenumbers.country_code_for_region(country_code)
+            except phonenumbers.phonenumberutil.Error:
+                pass
+
+        COUNTRY_DATA[country_code] = country_data
+
+    # Alphabetical sorting of the dictionary
+    sorted_countries = sorted(COUNTRY_DATA.items(), key=lambda x: x[1]['code'])
+
+    return sorted_countries
+
+@app.route('/countries', methods=['GET'])
+def get_countries():
+    sorted_countries = generate_country_data()
+    return jsonify(sorted_countries)
 
